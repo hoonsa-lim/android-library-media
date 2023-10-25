8ae47b20da50af13c56317ca56f5e94580ceb582
hslim <myohoon95@gmail.com>
2023-10-25 23:55:31 +0900

    add media-picker module

---
+++ media-picker/src/main/java/com/anythingsskyblue/android/library/media/media_picker/MediaPicker.kt
@@ -0,0 +1,35 @@
+package com.anythingsskyblue.android.library.media.media_picker
+
+import android.content.Context
+import android.net.Uri
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import com.anythingsskyblue.android.library.ui.util.ContextUtil
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+object MediaPicker {
+    private const val KEY_IMAGE_PICKER = "show_image_picker"
+    private const val KEY_VIDEO_PICKER = "show_video_picker"
+
+    suspend fun showImagePicker(context: Context): Uri? = suspendCancellableCoroutine{ continuation ->
+        val activity = ContextUtil.findComponentActivity(context)
+        val callback : (Uri?) -> Unit = { uri -> continuation.resume(uri) }
+
+        val pickMedia = activity
+            .activityResultRegistry
+            .register(KEY_IMAGE_PICKER, ActivityResultContracts.PickVisualMedia(), callback)
+
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    suspend fun showVideoPicker(context: Context): Uri? = suspendCancellableCoroutine{ continuation ->
+        val activity = ContextUtil.findComponentActivity(context)
+        val callback : (Uri?) -> Unit = { uri -> continuation.resume(uri) }
+
+        val pickMedia = activity
+            .activityResultRegistry
+            .register(KEY_VIDEO_PICKER, ActivityResultContracts.PickVisualMedia(), callback)
+
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,11 +9,12 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        maven { url = uri("https://www.jitpack.io") }
         mavenCentral()
+        maven { url = uri("https://www.jitpack.io") }
     }
 }
 
 rootProject.name = "android-library-media"
 include(":app")
 include(":metadata")
+include(":media-picker")
